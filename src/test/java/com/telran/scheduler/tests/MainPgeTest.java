724af3e3edc25220424130b3deadf1c4631fa05c
Liubov Selesh-Bilous <seleshbilousl@gmail.com>
2022-09-11 16:07:34 +0200

    First commit

---
+++ src/test/java/com/telran/scheduler/tests/MainPgeTest.java
@@ -0,0 +1,14 @@
+package com.telran.scheduler.tests;
+
+import org.testng.annotations.Test;
+
+public class MainPgeTest extends TestBase{
+
+    @Test
+    public void launchAppTest () {
+        System.out.println("App opened!");
+
+    }
+
+
+}
